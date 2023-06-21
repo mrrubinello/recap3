56002c771cba145d3b244157056f2bb3c85bb243
Bilal4317 <Sheriff43!?>
2023-06-21 11:15:19 +0200

    createcharactercard

--- components/card/card.js
+++ components/card/card.js
@@ -1,3 +1,25 @@
-export function createCharacterCard() {
-  console.log("test");
+const cardList = document.querySelector('[data-js="card-container"]');
+export function createCharacterCard(image, name, status, type, Occurrences) {
+  let card = `<li class="card">
+    <div class="card__image-container">
+      <img
+        class="card__image"
+        src="${image}"
+        alt="Rick Sanchez"
+      />
+      <div class="card__image-gradient"></div>
+    </div>
+    <div class="card__content">
+      <h2 class="card__title">${name}</h2>
+      <dl class="card__info">
+        <dt class="card__info-title">Status</dt>
+        <dd class="card__info-description">${status}</dd>
+        <dt class="card__info-title">Type</dt>
+        <dd class="card__info-description">${type}</dd>
+        <dt class="card__info-title">Occurrences</dt>
+        <dd class="card__info-description">${Occurrences}</dd>
+      </dl>
+    </div>
+  </li>`;
+  cardList.innerHTML = card;
 }
